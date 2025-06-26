f437cf000473777dce362f90fcb7dc984066c5f7
williamsw21 <westontwilliams25@gmail.com>
2025-06-26 11:02:41 -0400

    Added simple scatterplot of AAA K% vs BB%

--- AAA_hitters.R
+++ AAA_hitters.R
@@ -1,6 +1,7 @@
 library(dplyr)
 library(httr)
 library(jsonlite)
+library(ggplot2)
 AAA_hit_api <- "https://www.fangraphs.com/api/leaders/minor-league/data?pos=all&level=1&lg=2,4,5,6,7,8,9,10,11,14,12,13,15,16,17,18,30,32&stats=bat&qual=y&type=0&team=&season=2025&seasonEnd=2025&org=&ind=0&splitTeam=false"
 r <- GET(AAA_hit_api)
 AAA_hit <- fromJSON(content(r, as = "text"))
@@ -8,3 +9,6 @@ AAA_hit <- as.data.frame(AAA_hit)
 AAA_hit_final <- AAA_hit %>%
   select(PlayerName, TeamName, `K%`, `BB%`) %>%
   arrange(desc(`BB%`))
+
+ggplot(data = AAA_hit_final, mapping = aes(x = `K%`, y = `BB%`)) + 
+  geom_point()
